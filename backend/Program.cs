04d0e755f3fd8373a6924bfc062b630297624b26
Lucas Morais Gomes <lucasmorais947@gmail.com>
2026-02-24 17:28:47 -0300

    adaptando codigo para colocar no ar

--- backend/Program.cs
+++ backend/Program.cs
@@ -7,6 +7,8 @@ using PasswordManagerAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseUrls($"http://*:{Environment.GetEnvironmentVariable("PORT") ?? "8080"}");
+
 // 1. Banco de Dados
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
